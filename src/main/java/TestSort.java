58b7737da399828821dd9d672b202b088b925bc0
wangshunru <1436534683@qq.com>
2020-03-08 10:46:12 +0800

    sort test

--- src/main/java/TestSort.java
+++ src/main/java/TestSort.java
@@ -20,7 +20,10 @@ public class TestSort {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }*/
+
        //快速排序
+
+
        int[] arr = {49,38,65,97,23,22,76,1,5,8,2,0,-1,22};
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
