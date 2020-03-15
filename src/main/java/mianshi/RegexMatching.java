7b58310582117c5d79448aa8b748c3c11d11a9bd
王顺喜 <wangshunxi@ele-cloud.com>
2020-03-15 22:48:18 +0800

    正则匹配原理

--- src/main/java/mianshi/RegexMatching.java
+++ src/main/java/mianshi/RegexMatching.java
@@ -19,7 +19,7 @@ public class RegexMatching {
         matrix[0][0] = true;
         // 匹配第0行
         for (int i = 1; i < matrix[0].length; i++) {
-            // 被匹配的字符是* 向前取两个
+            // 被匹配的字符是* 向左取两个
             if (pattern[i - 1] == '*') {
                 matrix[0][i] = matrix[0][i - 2];
             }
@@ -29,20 +29,22 @@ public class RegexMatching {
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 1; j < matrix[0].length; j++) {
                 if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1]) {
-                    // 匹配到. 或 字符相等 ij的值 取 i-1 j-1的值
+                    // . 匹配任意字符 即与任意text中的字符匹配 || pattern中的字符与text中的字符相等 ,取值 左上方的值
                     matrix[i][j] = matrix[i - 1][j - 1];
                 } else if (pattern[j - 1] == '*') {
-                    // 匹配到* 向前取两个
+                    // 匹配到* 取值 左边偏移2的值
                     matrix[i][j] = matrix[i][j - 2];
                     if (pattern[j - 2] == '.' || pattern[j - 2] == text[i - 1]) {
-                        // 前一个为. 或 字符相等 当前值与前一个有一个为true就是true
+                        // 匹配到* 并且 左边一个的pattern字符与text中的字符相等 || pattern字符为.(.匹配任意字符) ,取值 上方的值为true 则为true
                         matrix[i][j] = matrix[i][j] | matrix[i - 1][j];
                     }
                 } else {
+                    // 不相等 pattern既不是.也不是*
                     matrix[i][j] = false;
                 }
             }
         }
+        // 返回最终匹配的结果
         return matrix[text.length][pattern.length];
     }
 }
