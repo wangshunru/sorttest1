3bbc72e67063439ef7f02a0d386c138384851c22
wangshunru <1436534683@qq.com>
2020-04-18 21:21:53 +0800

    917 仅仅反转数组

---
+++ src/main/java/leetcode/ReverseOnlyLetters.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+/**
+ * Created by wangshunxi on 2020/4/18.
+ * e-mail: wsr12240512@163.com
+ * phone: 13388071923
+ */
+/*给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置发生反转。
+*
+* */
+public class ReverseOnlyLetters {
+    public String reverseOnlyLetters(String S) {
+        char[] chars = S.toCharArray();
+        if (chars.length < 2) return S;
+        int left = 0;
+        int right = chars.length;
+        while (left < right){
+            while (left < right && !isChar(chars[left])) left++;
+            while (left < right && !isChar(chars[right])) right--;
+            if (left < right){
+                chars[left] ^= chars[right];
+                chars[right] ^= chars[left];
+                chars[left] ^= chars[right];
+                left ++;
+                right--;
+            }
+        }
+        return new String(chars);
+    }
+    public boolean isChar(char c){
+        return  (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+    }
+}
