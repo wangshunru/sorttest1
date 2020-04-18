045d16dbcd6244d8934d0e274fdfdc112eb093fd
wangshunru <1436534683@qq.com>
2020-04-18 21:31:08 +0800

    26 删除排序数组中的重复元素

---
+++ src/main/java/leetcode/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * Created by wangshunxi on 2020/4/18.
+ * e-mail: wsr12240512@163.com
+ * phone: 13388071923
+ */
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,3,4,3,4,5,5,5};
+        System.out.println(nums);
+    }
+    public static int removeDuplicates(int[] nums){
+        int slow = 0;
+        int fast = 0;
+        while(slow < fast){
+            if (nums[slow] == nums[fast]){
+                fast++;
+                continue;
+            }
+            slow++;
+            nums[slow] = nums[fast];
+            fast++;
+        }
+        return slow+1;
+    }
+}
